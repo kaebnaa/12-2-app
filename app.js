411eb0d664d0d13251fcf1b0c5b378154a0a4c16
sukhe <suhbat8575@gmail.com>
2023-04-26 08:21:25 +0800

    project v0.1.0

---
+++ app.js
@@ -0,0 +1,16 @@
+// * 
+// * * 
+// * * * 
+
+const x = document.querySelector("#inp");
+
+let b = "";
+
+for(let i=0; i<3; i++){
+    for(let j=0; j<3; j++){
+        b += " *";
+    }
+    b += "\n";
+}
+
+console.log(b);
